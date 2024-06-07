d5c4b81ef288ea9e0281d7a3c35690a82f0caa6d
sewonInst <sinogreee@gamil.com>
2024-06-07 17:25:53 +0900

    re2

--- src/main/java/edu/kh/project/member/controller/MemberController.java
+++ src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package edu.kh.project.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.kh.project.member.model.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -10,4 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("member")
 public class MemberController {
 
+	@Autowired 
+	private MemberService service;
 }
